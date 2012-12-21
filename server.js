9c44f54f9d22a8239ff2eb87ff128a3c9e51cb1b
Julien Chaumond <chaumond@gmail.com>
2012-12-21 19:21:29 +0100

    Let's try mongoose

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 
-var redis = require('redis');
+var redis = require('redis'),
+	mongoose = require('mongoose');
+
 
 var redisClient = redis.createClient();
 
@@ -7,16 +9,37 @@ redisClient.on("error", function(err){
 	console.log("Error " + err);
 });
 
+mongoose.connect('localhost', 'hn');
+
+var NewsSchema = mongoose.Schema({
+	id: String,
+	title: String,
+	url: String,
+	domain: String,
+	points: Number,
+	user: String,
+	time_ago: String,
+	comments_count: Number,
+	type: String
+});
+
+var News = mongoose.model('News', NewsSchema);
 
 
 setInterval(function(){
 	redisClient.get('news', function(err, result){
 		if (result){
 			var news = JSON.parse(result);
-			console.log(typeof news);
+			console.log(news.length);
+			news.forEach(function(item){
+				new News(item).save();
+			});
 		}
-		// If there isn't anything (i.e. the TTL for the Redis value has expired), 
-		// we don't update the Mongo collection.
+		else {
+			// If there isn't anything (i.e. the TTL for the Redis value has expired), 
+			// we don't update the Mongo collection.
+			console.log('ERR');
+		}
 	});
 }, 5000);
 
