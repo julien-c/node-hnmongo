39b80aef170e25691c71ad206376fe7ef1380539
Julien Chaumond <chaumond@gmail.com>
2012-12-21 18:16:48 +0100

    get news from Redis

--- server.js
+++ server.js
@@ -1,9 +1,23 @@
 
 var redis = require('redis');
 
+var redisClient = redis.createClient();
+
+redisClient.on("error", function(err){
+	console.log("Error " + err);
+});
+
+
 
 setInterval(function(){
-	console.log("test");
+	redisClient.get('news', function(err, result){
+		if (result){
+			var news = JSON.parse(result);
+			console.log(typeof news);
+		}
+		// If there isn't anything (i.e. the TTL for the Redis value has expired), 
+		// we don't update the Mongo collection.
+	});
 }, 5000);
 
 
