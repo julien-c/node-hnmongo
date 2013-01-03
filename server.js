4497e7683030dfa302a84dc2dc6977e4f55b2da6
Julien Chaumond <chaumond@gmail.com>
2013-01-03 18:29:14 +0100

    remove redis and mongoose drivers

--- server.js
+++ server.js
@@ -1,56 +1,36 @@
 
-var redis = require('redis');
-var mongoose = require('mongoose');
+
 var http = require('http');
 
 
-var redisClient = redis.createClient();
 
-redisClient.on("error", function(err){
-	console.log("Error " + err);
-});
-
-mongoose.connect('localhost', 'hn');
-
-var NewsSchema = mongoose.Schema({
-	id: {type: String, unique: true},
-	title: String,
-	url: String,
-	domain: String,
-	points: Number,
-	user: String,
-	time_ago: String,
-	comments_count: Number,
-	type: String
-});
-
-var News = mongoose.model('News', NewsSchema);
 
 
 setInterval(function(){
 	http.get("http://localhost:8888/news", function(res) {
 		console.log("Got response: " + res.statusCode);
+		console.log(res.length);
 	}).on('error', function(e) {
 		console.log("Got error: " + e.message);
 	});
 	
-	redisClient.get('news', function(err, result){
-		if (result) {
-			var news = JSON.parse(result);
-			console.log(news.length);
+	// redisClient.get('news', function(err, result){
+	// 	if (result) {
+	// 		var news = JSON.parse(result);
+	// 		console.log(news.length);
 			
 			
-			// news.forEach(function(item){
-			// 	new News(item).save();
-			// });
-		}
-		else {
-			// If there isn't anything (i.e. the TTL for the Redis value has expired), 
-			// we don't update the Mongo collection.
-			console.log('ERR');
-		}
-	});
-}, 20000);
+	// 		// news.forEach(function(item){
+	// 		// 	new News(item).save();
+	// 		// });
+	// 	}
+	// 	else {
+	// 		// If there isn't anything (i.e. the TTL for the Redis value has expired), 
+	// 		// we don't update the Mongo collection.
+	// 		console.log('ERR');
+	// 	}
+	// });
+}, 3000);
 
 
 
