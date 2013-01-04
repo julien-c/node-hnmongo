90de2df2f8aece5159eb77bfa3f703344bfa6ede
Julien Chaumond <chaumond@gmail.com>
2013-01-04 12:17:59 +0100

    Also store individual items

--- server.js
+++ server.js
@@ -11,6 +11,7 @@ MongoClient.connect("mongodb://localhost:27017/hnmeteor", function(err, db) {
 	if (err) { return console.dir(err); }
 	
 	var homepage = db.collection('homepage');
+	var item     = db.collection('item');
 	
 	setInterval(function(){
 		http.get("http://localhost:8888/news", function(res) {
@@ -24,6 +25,22 @@ MongoClient.connect("mongodb://localhost:27017/hnmeteor", function(err, db) {
 			res.on('end', function(){
 				var home = JSON.parse(body);
 				homepage.insert({content: home}, {w:1}, function(err, result) {});
+			});
+		}).on('error', function(e) {
+			console.log("Got error: " + e.message);
+		});
+		
+		http.get("http://localhost:8888/item/5004294", function(res) {
+			console.log(res.statusCode);
+			
+			var body = '';
+			res.setEncoding('utf8');
+			res.on('data', function(chunk){
+				body += chunk;
+			});
+			res.on('end', function(){
+				var content = JSON.parse(body);
+				item.insert({content: content}, {w:1}, function(err, result) {});
 			});
 		}).on('error', function(e) {
 			console.log("Got error: " + e.message);
