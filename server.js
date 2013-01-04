391f0bf2fb4378e91f205df0ad1e764b21eb4193
Julien Chaumond <chaumond@gmail.com>
2013-01-04 14:34:57 +0100

    Comment item out

--- server.js
+++ server.js
@@ -32,21 +32,21 @@ MongoClient.connect("mongodb://localhost:27017/hnmeteor", function(err, db) {
 			console.log("Got error: " + e.message);
 		});
 		
-		http.get("http://localhost:8888/item/5006967", function(res) {
-			console.log(res.statusCode);
+		// http.get("http://localhost:8888/item/5006967", function(res) {
+		// 	console.log(res.statusCode);
 			
-			var body = '';
-			res.setEncoding('utf8');
-			res.on('data', function(chunk){
-				body += chunk;
-			});
-			res.on('end', function(){
-				var content = JSON.parse(body);
-				item.insert({content: content}, {w:1}, function(err, result) {});
-			});
-		}).on('error', function(e) {
-			console.log("Got error: " + e.message);
-		});
+		// 	var body = '';
+		// 	res.setEncoding('utf8');
+		// 	res.on('data', function(chunk){
+		// 		body += chunk;
+		// 	});
+		// 	res.on('end', function(){
+		// 		var content = JSON.parse(body);
+		// 		item.insert({content: content}, {w:1}, function(err, result) {});
+		// 	});
+		// }).on('error', function(e) {
+		// 	console.log("Got error: " + e.message);
+		// });
 		
 	}, 10000);
 });
