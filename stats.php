b5fbae6b3390938e18c581838b41cf5f6bc2ed3e
Julien Chaumond <chaumond@gmail.com>
2013-01-04 12:17:44 +0100

    Prettify JSON

--- stats.php
+++ stats.php
@@ -15,6 +15,7 @@ foreach ($c as $h) {
 		else {
 			echo "D";
 			$different++;
+			file_put_contents($i.'.json', pretty_json(json_encode($h['content'])));
 		}
 		// file_put_contents($i.'.json', json_encode($h['content']));
 	}
@@ -26,3 +27,65 @@ foreach ($c as $h) {
 echo "\n";
 echo sprintf("Different: %d out of %d", $different, $i);
 echo "\n";
+
+
+
+
+
+
+
+
+
+/******/
+
+
+function pretty_json($json) {
+
+    $result      = '';
+    $pos         = 0;
+    $strLen      = strlen($json);
+    $indentStr   = '  ';
+    $newLine     = "\n";
+    $prevChar    = '';
+    $outOfQuotes = true;
+
+    for ($i=0; $i<=$strLen; $i++) {
+
+        // Grab the next character in the string.
+        $char = substr($json, $i, 1);
+
+        // Are we inside a quoted string?
+        if ($char == '"' && $prevChar != '\\') {
+            $outOfQuotes = !$outOfQuotes;
+        
+        // If this character is the end of an element, 
+        // output a new line and indent the next line.
+        } else if(($char == '}' || $char == ']') && $outOfQuotes) {
+            $result .= $newLine;
+            $pos --;
+            for ($j=0; $j<$pos; $j++) {
+                $result .= $indentStr;
+            }
+        }
+        
+        // Add the character to the result string.
+        $result .= $char;
+
+        // If the last character was the beginning of an element, 
+        // output a new line and indent the next line.
+        if (($char == ',' || $char == '{' || $char == '[') && $outOfQuotes) {
+            $result .= $newLine;
+            if ($char == '{' || $char == '[') {
+                $pos ++;
+            }
+            
+            for ($j = 0; $j < $pos; $j++) {
+                $result .= $indentStr;
+            }
+        }
+        
+        $prevChar = $char;
+    }
+
+    return $result;
+}
